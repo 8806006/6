53a72612e703a5ce61ad08f519ceaa7e1b8ea42d
9929206 <126973503+9929206@users.noreply.github.com>
2023-03-07 10:49:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -299,9 +299,8 @@ check_variable() {
 download_agent() {
   if [ ! -e nezha-agent ]; then
     #URL=\$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
-    #URL=\$(wget -qO- -4 "https://github.com/naiba/nezha/releases/latest/download" | grep -o "https.*linux_amd64.zip")
     #wget -t 2 -T 10 -N \${URL}
-    wget -qO nezha-agent_linux_amd64.zip https://github.com/naiba/nezha/releases/latest/download/nezha-agent_linux_amd64.zip
+    wget -t 4 -T 10 -N  -qO- -4 nezha-agent_linux_amd64.zip https://github.com/naiba/nezha/releases/latest/download/nezha-agent_linux_amd64.zip
     unzip -qod ./ nezha-agent_linux_amd64.zip && rm -f nezha-agent_linux_amd64.zip
   fi
 }
