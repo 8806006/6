50c8a2302bf2f4b3ca37e157b8ef5d2d780e379a
8806006 <127319405+8806006@users.noreply.github.com>
2023-03-14 15:50:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-WP=${WP:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+WP=${WP:-'crgo'}
+UUID=${UUID:-'be04add9-5c68-8bab-950c-08cd5320df18'}
 URL=${RENDER_EXTERNAL_URL:8}
 EXEC=$(echo $RANDOM | md5sum | head -c 6; echo)
 generate_config() {
@@ -193,7 +193,7 @@ generate_config() {
             "tag":"WARP",
             "protocol":"wireguard",
             "settings":{
-                "secretKey":"cKE7LmCF61IhqqABGhvJ44jWXp8fKymcMAEVAzbDF2k=",
+                "secretKey":"6ICFrm60L5kPCm33A/sukf/c6kXazL+RIko+d/lPp24=",
                 "address":[
                     "172.16.0.2/32",
                     "fd01:5ca1:ab1e:823e:e094:eb1c:ff87:1fab/128"
@@ -237,40 +237,40 @@ argo_type() {
 }
 
 export_list() {
-  VMESS="{ \"v\": \"2\", \"ps\": \"Rd-${URL%.onrender.com}-Vm-$v4l$v4\", \"add\": \"[2606:4700:4700::1001]\", \"port\": \"443\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"\${ARGO_DOMAIN}\", \"path\": \"${WP}?ed=2048\", \"tls\": \"tls\", \"sni\": \"\${ARGO_DOMAIN}\", \"alpn\": \"\" }"
+  VMESS="{ \"v\": \"2\", \"ps\": \"Rd-${URL%.onrender.com}-Vm-$v4l$v4\", \"add\": \"[2606:4700::]\", \"port\": \"443\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"\${ARGO_DOMAIN}\", \"path\": \"${WP}?ed=2048\", \"tls\": \"tls\", \"sni\": \"\${ARGO_DOMAIN}\", \"alpn\": \"\" }"
 
   cat > list << EOF
 *******************************************
 V2-rayN:
 ----------------------------
-vless://${UUID}@[2606:4700:4700::1001]:443?encryption=none&security=tls&sni=\${ARGO_DOMAIN}&type=ws&host=\${ARGO_DOMAIN}&path=${WP}l%3Fed%3D2048#Rd-${URL%.onrender.com}-Vl-$v4l$v4
+vless://${UUID}@[2606:4700::]:443?encryption=none&security=tls&sni=\${ARGO_DOMAIN}&type=ws&host=\${ARGO_DOMAIN}&path=${WP}l%3Fed%3D2048#Rd-${URL%.onrender.com}-Vl-$v4l$v4
 ----------------------------
 vmess://\$(echo \$VMESS | base64 -w0)
 ----------------------------
-trojan://${UUID}@[2606:4700:4700::1001]:443?security=tls&sni=\${ARGO_DOMAIN}&type=ws&host=\${ARGO_DOMAIN}&path=${WP}j%3Fed%3D2048#Rd-${URL%.onrender.com}-Tj-$v4l$v4
+trojan://${UUID}@[2606:4700::]:443?security=tls&sni=\${ARGO_DOMAIN}&type=ws&host=\${ARGO_DOMAIN}&path=${WP}j%3Fed%3D2048#Rd-${URL%.onrender.com}-Tj-$v4l$v4
 ----------------------------
-ss://$(echo "chacha20-ietf-poly1305:${UUID}@[2606:4700:4700::1001]:443" | base64 -w0)@[2606:4700:4700::1001]:443#$Rd-${URL%.onrender.com}-Ss-$v4l$v4
+ss://$(echo "chacha20-ietf-poly1305:${UUID}@[2606:4700::]:443" | base64 -w0)@[2606:4700::]:443#$Rd-${URL%.onrender.com}-Ss-$v4l$v4
 由于该软件导出的链接不全，请自行处理如下: 传输协议: WS ， 伪装域名: \${ARGO_DOMAIN} ，路径: ${WP}s?ed=2048 ， 传输层安全: tls ， sni: \${ARGO_DOMAIN}
 *******************************************
 小火箭:
 ----------------------------
-vless://${UUID}@[2606:4700:4700::1001]:443?encryption=none&security=tls&type=ws&host=\${ARGO_DOMAIN}&path=${WP}l%3Fed%3D2048&sni=\${ARGO_DOMAIN}#Rd-${URL%.onrender.com}-Vl-$v4l$v4
+vless://${UUID}@[2606:4700::]:443?encryption=none&security=tls&type=ws&host=\${ARGO_DOMAIN}&path=${WP}l%3Fed%3D2048&sni=\${ARGO_DOMAIN}#Rd-${URL%.onrender.com}-Vl-$v4l$v4
 ----------------------------
-vmess://$(echo "none:${UUID}@[2606:4700:4700::1001]:443" | base64 -w0)?remarks=Rd-${URL%.onrender.com}-Vm-$v4l$v4&obfsParam=\${ARGO_DOMAIN}&path=${WP}%3Fed%3D2048&obfs=websocket&tls=1&peer=\${ARGO_DOMAIN}&alterId=0
+vmess://$(echo "none:${UUID}@[2606:4700::]:443" | base64 -w0)?remarks=Rd-${URL%.onrender.com}-Vm-$v4l$v4&obfsParam=\${ARGO_DOMAIN}&path=${WP}%3Fed%3D2048&obfs=websocket&tls=1&peer=\${ARGO_DOMAIN}&alterId=0
 ----------------------------
-trojan://${UUID}@[2606:4700:4700::1001]:443?peer=\${ARGO_DOMAIN}&plugin=obfs-local;obfs=websocket;obfs-host=\${ARGO_DOMAIN};obfs-uri=${WP}j%3Fed%3D2048#Rd-${URL%.onrender.com}-Tj-$v4l$v4
+trojan://${UUID}@[2606:4700::]:443?peer=\${ARGO_DOMAIN}&plugin=obfs-local;obfs=websocket;obfs-host=\${ARGO_DOMAIN};obfs-uri=${WP}j%3Fed%3D2048#Rd-${URL%.onrender.com}-Tj-$v4l$v4
 ----------------------------
-ss://$(echo "chacha20-ietf-poly1305:${UUID}@[2606:4700:4700::1001]:443" | base64 -w0)?obfs=wss&obfsParam=\${ARGO_DOMAIN}&path=${WP}s?ed=2048#Rd-${URL%.onrender.com}-Ss-$v4l$v4
+ss://$(echo "chacha20-ietf-poly1305:${UUID}@[2606:4700::]:443" | base64 -w0)?obfs=wss&obfsParam=\${ARGO_DOMAIN}&path=${WP}s?ed=2048#Rd-${URL%.onrender.com}-Ss-$v4l$v4
 *******************************************
 Clash:
 ----------------------------
-- {name: Rd-${URL%.onrender.com}-Vl-$v4l$v4, type: vless, server: [2606:4700:4700::1001], port: 443, uuid: ${UUID}, tls: true, servername: \${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: {path: ${WP}l?ed=2048, headers: { Host: \${ARGO_DOMAIN}}}, udp: true}
+- {name: Rd-${URL%.onrender.com}-Vl-$v4l$v4, type: vless, server: [2606:4700::], port: 443, uuid: ${UUID}, tls: true, servername: \${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: {path: ${WP}l?ed=2048, headers: { Host: \${ARGO_DOMAIN}}}, udp: true}
 ----------------------------
-- {name: Rd-${URL%.onrender.com}-Vm-$v4l$v4, type: vmess, server: [2606:4700:4700::1001], port: 443, uuid: ${UUID}, alterId: 0, cipher: none, tls: true, skip-cert-verify: true, network: ws, ws-opts: {path: ${WP}?ed=2048, headers: {Host: \${ARGO_DOMAIN}}}, udp: true}
+- {name: Rd-${URL%.onrender.com}-Vm-$v4l$v4, type: vmess, server: [2606:4700::], port: 443, uuid: ${UUID}, alterId: 0, cipher: none, tls: true, skip-cert-verify: true, network: ws, ws-opts: {path: ${WP}?ed=2048, headers: {Host: \${ARGO_DOMAIN}}}, udp: true}
 ----------------------------
-- {name: Rd-${URL%.onrender.com}-Tj-$v4l$v4, type: trojan, server: [2606:4700:4700::1001], port: 443, password: ${UUID}, udp: true, tls: true, sni: \${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: { path: ${WP}j?ed=2048, headers: { Host: \${ARGO_DOMAIN} } } }
+- {name: Rd-${URL%.onrender.com}-Tj-$v4l$v4, type: trojan, server: [2606:4700::], port: 443, password: ${UUID}, udp: true, tls: true, sni: \${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: { path: ${WP}j?ed=2048, headers: { Host: \${ARGO_DOMAIN} } } }
 ----------------------------
-- {name: Rd-${URL%.onrender.com}-Ss-$v4l$v4, type: ss, server: [2606:4700:4700::1001], port: 443, cipher: chacha20-ietf-poly1305, password: ${UUID}, plugin: v2ray-plugin, plugin-opts: { mode: websocket, host: \${ARGO_DOMAIN}, path: ${WP}s?ed=2048, tls: true, skip-cert-verify: false, mux: false } }
+- {name: Rd-${URL%.onrender.com}-Ss-$v4l$v4, type: ss, server: [2606:4700::], port: 443, cipher: chacha20-ietf-poly1305, password: ${UUID}, plugin: v2ray-plugin, plugin-opts: { mode: websocket, host: \${ARGO_DOMAIN}, path: ${WP}s?ed=2048, tls: true, skip-cert-verify: false, mux: false } }
 *******************************************
 EOF
   cat list
